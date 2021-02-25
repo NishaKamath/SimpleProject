d9eaca7b770d823858dab7fac7a199ae2fec6b75
NishaKamath <67003977+NishaKamath@users.noreply.github.com>
2021-02-25 15:31:38 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        protected void display()
+        {
+        	System.out.println("This is from superclass");
+        }
+	}
+
+}
